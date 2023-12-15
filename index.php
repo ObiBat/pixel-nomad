98952d750113e92ade6cb9f60ea5632a82816d8f
ObiBat <22037760@student.westernsydney.edu.au>
2023-12-15 19:23:50 +1100

    Adding Php mailer manually added

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-require 'vendor/autoload.php';
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
